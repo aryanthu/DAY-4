f5840db2dc43f2dba88d7be774e4c3e78a427a55
juanwyck70 <juanwyck69420@gmail.com>
2026-06-26 17:23:36 +0530

    Deploy production build

--- src/app/accordion/accordion.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-accordion',
-  imports: [],
-  templateUrl: './accordion.html',
-  styleUrl: './accordion.css',
-})
-export class Accordion {}
